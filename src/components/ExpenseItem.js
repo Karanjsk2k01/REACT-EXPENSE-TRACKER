da99826e70042b3abaca1b15a7f370147aa5a900
Karanjsk2k01 <karanvarun14501@gmail.com>
2023-12-17 02:39:28 +0530

    Added Data as an object ans used map function

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,23 @@
+import './ExpenseItem.css';
 
-function Expense() {
+function Expense(props) {
+  // const date = new Date(2023, 2, 28);
+  // const expenses = "Car Insurance";
+  // const expense_price = "$294.95";
+  // const LocationOfExpenses = "AutoMobiles";
+
   return (
-    <div>
-      <h3>Expenses Items</h3>
-      <p>Food Rs 10</p>
-      <p>Petrol Rs 100</p>
-      <p>Movies Rs 200</p>
+    <div className="expense-item">
+      <div>
+        <h2>{props.date.toISOString()}</h2>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <h2>{props.locationOfExpenditure}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+        <div></div>
+      </div>
+
     </div>
   )
 }
