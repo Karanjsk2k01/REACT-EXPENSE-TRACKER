17353aa45b753c8d9327c8073e759dcc7a91e011
Karanjsk2k01 <karanvarun14501@gmail.com>
2023-12-17 01:30:55 +0530

    created react app and hardcodes expenses data

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+
+function Expense() {
+  return (
+    <div>
+      <h3>Expenses Items</h3>
+      <p>Food Rs 10</p>
+      <p>Petrol Rs 100</p>
+      <p>Movies Rs 200</p>
+    </div>
+  )
+}
+
+export default Expense;
