376732d8c6b856574bc052a0616bccebb2596db8
Karanjsk2k01 <karanvarun14501@gmail.com>
2023-12-17 03:08:20 +0530

    restructured and reduced the code through different components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,16 @@
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
 
 function Expense(props) {
-  // const date = new Date(2023, 2, 28);
-  // const expenses = "Car Insurance";
-  // const expense_price = "$294.95";
-  // const LocationOfExpenses = "AutoMobiles";
-
   return (
     <div className="expense-item">
-      <div>
-        <h2>{props.date.toISOString()}</h2>
-      </div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <h2>{props.locationOfExpenditure}</h2>
-        <div className="expense-item__price">{props.amount}</div>
-        <div></div>
-      </div>
-
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        amount={props.amount}
+        locationOfExpenditure={props.locationOfExpenditure}
+        title={props.title}
+      />
     </div>
   )
 }
