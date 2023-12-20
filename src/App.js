94a9bf5be2c21bbaebeeab3f5a7e6cacfdaa5c5b
Karanjsk2k01 <karanvarun14501@gmail.com>
2023-12-20 14:10:34 +0530

    Added Expense Form in the UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from './components/NewExpense/NewExpense';
 
 
 var App = () => {
   return (
     <div>
+      <NewExpense />
       <Expenses />
     </div>
   )
