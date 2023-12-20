f88e477bea5e89cfe2cf65b029514541242e6ead
Karanjsk2k01 <karanvarun14501@gmail.com>
2023-12-20 13:29:31 +0530

    practised useState hook indepth by changing the state of the component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,22 +2,31 @@ import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
 import Card from '../UI/card';
+import { useState } from 'react';
 
 var ExpenseItem = (props) => {
 
-  const clickHandlers = () => {
+  let [title, setTitle] = useState(props.title);
+  const [expense, setExpense] = useState(props.amount);
 
-    console.log('clicked');
+  const updateExpense = () => {
+    setExpense('$100');
+  }
+
+  const clickHandlers = () => {
+    setTitle('Updated');
+    console.log(title);
   }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails
-        amount={props.amount}
+        amount={expense}
         locationOfExpenditure={props.locationOfExpenditure}
-        title={props.title}
+        title={title}
       />
+      <button onClick={updateExpense}>Update Expense Amount</button>
       <button onClick={clickHandlers}>DeleteExpenses</button>
     </Card>
   )
