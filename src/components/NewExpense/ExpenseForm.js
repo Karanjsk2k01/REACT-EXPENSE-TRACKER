c3ad8abc5be9a2bd8af724ff3a40288e8e568d2a
Karanjsk2k01 <karanvarun14501@gmail.com>
2023-12-23 11:38:39 +0530

    Added contitional rendering for the form buttons

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -7,6 +7,7 @@ const ExpenseForm = (props) => {
   const [enteredtitle, setenteredtitle] = useState('');
   const [enteredAmount, setenteredAmount] = useState('');
   const [enteredDate, setenteredDate] = useState('');
+  const [isEditing, setIsEditing] = useState(false);
 
 
   const enteredtitleHandler = (e) => {
@@ -37,27 +38,53 @@ const ExpenseForm = (props) => {
     setenteredDate('');
   }
 
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const cancelEditingHandler = () => {
+    // Clearing the inputs
+    setenteredtitle('');
+    setenteredAmount('');
+    setenteredDate('');
+    // Exiting edit mode without saving
+    setIsEditing(false);
+  };
+
   return (
-    <form onSubmit={enteredDetails}>
-      <div className='new-expense__controls'>
-        <div className='new-expense__control'>
-          <label>Title</label>
-          <input type='text' required value={enteredtitle} onChange={enteredtitleHandler} />
+    <div>
+      {isEditing ? (
+        <form onSubmit={enteredDetails}>
+          <div className='new-expense__controls'>
+            <div className='new-expense__control'>
+              <label>Title</label>
+              <input type='text' required value={enteredtitle} onChange={enteredtitleHandler} />
+            </div>
+            <div className='new-expense__control'>
+              <label>Amount</label>
+              <input type='number' required min='0' step='1' value={enteredAmount} onChange={enteredAmountHandler} />
+            </div>
+            <div className='new-expense__control'>
+              <label>Date</label>
+              <input type='date' required min='2019-01-01' max='2023-12-31' value={enteredDate} onChange={enteredDateHandler} />
+            </div>
+          </div>
+          <div className='new-expense__actions'>
+            <button type='submit'>Add Expense</button>
+            <button type='button' onClick={cancelEditingHandler}>
+              Cancel
+            </button>
+          </div>
+        </form>
+      ) : (
+        <div className='new-expense__actions'>
+          <button type='button' onClick={startEditingHandler}>
+            Add Expense
+          </button>
         </div>
-        <div className='new-expense__control'>
-          <label>Amount</label>
-          <input type='number' required min="0" step="1" value={enteredAmount} onChange={enteredAmountHandler} />
-        </div>
-        <div className='new-expense__control'>
-          <label>Date</label>
-          <input type='date' required min="2019-01-01" max="2023-12-31" value={enteredDate} onChange={enteredDateHandler} />
-        </div>
-      </div>
-      <div className='new-expense__actions'>
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
-  )
+      )}
+    </div>
+  );
 }
 
 export default ExpenseForm;
